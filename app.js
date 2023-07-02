59adc8f79f7117c4cc4c0eed207ab95481be82ea
MohammedIbrahimSheikh <ibrahim03178871902@gmail.com>
2023-07-01 22:15:35 -0800

    added javaScript file

---
+++ app.js
@@ -0,0 +1,20 @@
+const quizSelector = document.getElementById("quiz-selector");
+const quizContainer = document.getElementById("quiz-container");
+const questionContainer = document.getElementById("question-container");
+const answerButtonContainer = document.getElementById("answer-buttons-container");
+const resultContainer = document.getElementById("results-container");
+
+const loadAllQuiz = async () => {
+    const response = await fetch("./quizzes.json");
+    const quizzes = await response.json();
+  
+    quizzes.forEach((quiz, index) => {
+      const quizCard = document.createElement("div");
+      quizCard.classList = ["quiz-card"];
+      quizCard.innerText = "Quiz " + (index + 1);
+      quizCard.addEventListener("click", () => loadQuiz(quiz));
+      quizSelector.appendChild(quizCard);
+    });
+  };
+  
+  loadAllQuiz();
